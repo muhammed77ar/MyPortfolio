2baa6cad5f1d50b3ce8dcd52b1591c268eae189d
Muhammad <mohamedar4477@gmail.com>
2024-10-05 22:24:26 +0200

    fix the loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 
 // Lazy load components
 const About = lazy(() => import('./pages/About'));
@@ -14,23 +14,37 @@ const ContactMe = lazy(() => import('./components/ContactMe'));
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <div className=" relative z-0 bg-bg-pattern bg-cover bg-no-repeat bg-top">
-        <div className=" bg-[url('/src/assets/HeroImage.png')] bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
+      <BrowserRouter>
+        <div className=" relative z-0 bg-bg-pattern bg-cover bg-no-repeat bg-top">
+          <div className=" bg-[url('/src/assets/HeroImage.png')] bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <Suspense fallback={
+            <div className="w-full gap-x-2 flex justify-center items-center">
+              <div
+                className="w-5 bg-[#d991c2] animate-pulse h-5 rounded-full animate-bounce"
+              ></div>
+              <div
+                className="w-5 animate-pulse h-5 bg-[#9869b8] rounded-full animate-bounce"
+              ></div>
+              <div
+                className="w-5 h-5 animate-pulse bg-[#6756cc] rounded-full animate-bounce"
+              ></div>
+            </div>
+          }>
+            <About />
+            <Experience />
+            <Tech />
+            <Works />
+            <ContactMe />
+          </Suspense>
+          <div>
+          </div>
         </div>
-          <About />
-          <Experience />
-          <Tech />
-          <Works />
-          <ContactMe />
-        <div>
-        </div>
-      </div>
-    </BrowserRouter>
-    
- 
+      </BrowserRouter>
+
+
     </>
   )
 }
